d73779e31c1782a04ce2e8a9dc6aa9f4dc9f5dbe
Daniel <kingsantha16af@gmail.com>
2020-06-09 19:52:32 +0700

    Pasang Future Builder di halaman detail ember

--- lib/detail_ember.dart
+++ lib/detail_ember.dart
@@ -28,114 +28,118 @@ class _DetailEmberState extends State<DetailEmber> {
       body: SingleChildScrollView(
         child: Container(
           margin: EdgeInsets.all(16.0),
-          child: FutureBuilder(
-            future: getDetailEmber(bucketId),
-            builder: (context, snapshot){
-              if(snapshot.data==null){
-                return Container();
-              }else{
-                Data ember = snapshot.data.data;
-                return Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                width: MediaQuery.of(context).size.width * 100,
+                height: MediaQuery.of(context).size.height * 0.12,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: <Widget>[
-                    Container(
-                      width: MediaQuery.of(context).size.width * 100,
-                      height: MediaQuery.of(context).size.height * 0.12,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: <Widget>[
-                          Image.asset('assets/Logo.png'),
-                          Text(
-                            'eBudikdamber',
-                            style: TextStyle(
-                                fontSize: 26.0, fontWeight: FontWeight.bold),
-                          ),
-                          SizedBox(
-                            width: 12.0,
-                          ),
-                          Image.asset('assets/Menu.png'),
-                        ],
-                      ),
-                    ),
-                    Container(
-                        width: MediaQuery.of(context).size.width * 0.25,
-                        height: MediaQuery.of(context).size.height * 0.12,
-                        decoration: new BoxDecoration(
-                            shape: BoxShape.circle,
-                            image: new DecorationImage(
-                                fit: BoxFit.fill,
-                                image: AssetImage('assets/Ember3.png')))),
-                    SizedBox(
-                      height: 8.0,
-                    ),
+                    Image.asset('assets/Logo.png'),
                     Text(
-                      ember.name,
-                      style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+                      'eBudikdamber',
+                      style: TextStyle(
+                          fontSize: 26.0, fontWeight: FontWeight.bold),
                     ),
                     SizedBox(
-                      height: 8.0,
+                      width: 12.0,
                     ),
-                    Container(
-                      height: MediaQuery.of(context).size.height*0.05,
-                      width: MediaQuery.of(context).size.width*0.25,
-                      child: FlatButton.icon(
-                        onPressed: () {},
-                        icon: Icon(Icons.edit),
-                        label: Text('Edit'),
-                        color: Color(0XFFffac30),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 24.0,
-                    ),
-                    TextField(
-                      decoration: InputDecoration(
-                          border: InputBorder.none,
-                          hintText: 'Keterangan 1',
-                          hintStyle: TextStyle(color: Colors.deepPurple)
-                      ),
-                    ),SizedBox(
-                      height: 12.0,
-                    ),
-                    TextField(
-                      decoration: InputDecoration(
-                          border: InputBorder.none,
-                          hintText: 'Keterangan 2',
-                          hintStyle: TextStyle(color: Colors.deepPurple)
-                      ),
-                    ),SizedBox(
-                      height: 12.0,
-                    ),
-                    TextField(
-                      decoration: InputDecoration(
-                          border: InputBorder.none,
-                          hintText: 'Keterangan 3',
-                          hintStyle: TextStyle(color: Colors.deepPurple)
-                      ),
-                    ),SizedBox(
-                      height: 12.0,
-                    ),
-                    TextField(
-                      decoration: InputDecoration(
-                          border: InputBorder.none,
-                          hintText: 'Keterangan 4',
-                          hintStyle: TextStyle(color: Colors.deepPurple)
-                      ),
-                    ),SizedBox(
-                      height: 28.0,
-                    ),ImageButton(
-                      children: <Widget>[],
-                      unpressedImage: Image.asset('assets/Button_simpan.png'),
-                      pressedImage: Image.asset('assets/Button_simpan.png'),
-                      height: 45.0,
-                      width: 150.0,
-                    )
+                    Image.asset('assets/Menu.png'),
                   ],
-                );
-              }
-            },
+                ),
+              ),
+              FutureBuilder(
+                future: getDetailEmber(bucketId),
+                builder: (context, snapshot){
+                  if(snapshot.data == null){
+                    return Container();
+                  }else{
+                    Data ember= snapshot.data.data;
+                    return Column(
+                      children: [
+                        Container(
+                            width: MediaQuery.of(context).size.width * 0.25,
+                            height: MediaQuery.of(context).size.height * 0.12,
+                            decoration: new BoxDecoration(
+                                shape: BoxShape.circle,
+                                image: new DecorationImage(
+                                    fit: BoxFit.fill,
+                                    image: AssetImage('assets/Ember3.png')))),
+                        SizedBox(
+                          height: 8.0,
+                        ),
+                        Text(
+                          ember.name,
+                          style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(
+                          height: 8.0,
+                        ),
+                        Container(
+                          height: MediaQuery.of(context).size.height*0.05,
+                          width: MediaQuery.of(context).size.width*0.25,
+                          child: FlatButton.icon(
+                            onPressed: () {},
+                            icon: Icon(Icons.edit),
+                            label: Text('Edit'),
+                            color: Color(0XFFffac30),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 24.0,
+                        ),
+                        TextField(
+                          decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: 'Keterangan 1',
+                              hintStyle: TextStyle(color: Colors.deepPurple)
+                          ),
+                        ),SizedBox(
+                          height: 12.0,
+                        ),
+                        TextField(
+                          decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: 'Keterangan 2',
+                              hintStyle: TextStyle(color: Colors.deepPurple)
+                          ),
+                        ),SizedBox(
+                          height: 12.0,
+                        ),
+                        TextField(
+                          decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: 'Keterangan 3',
+                              hintStyle: TextStyle(color: Colors.deepPurple)
+                          ),
+                        ),SizedBox(
+                          height: 12.0,
+                        ),
+                        TextField(
+                          decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: 'Keterangan 4',
+                              hintStyle: TextStyle(color: Colors.deepPurple)
+                          ),
+                        ),SizedBox(
+                          height: 28.0,
+                        ),ImageButton(
+                          children: <Widget>[],
+                          unpressedImage: Image.asset('assets/Button_simpan.png'),
+                          pressedImage: Image.asset('assets/Button_simpan.png'),
+                          height: 45.0,
+                          width: 150.0,
+                        )
+                      ],
+                    );
+                  }
+                },
+              )
+            ],
           ),
         ),
       ),
